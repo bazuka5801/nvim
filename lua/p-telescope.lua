7662dfb5567bf7bf481d103f84ccc2a3448063f5
Maxim Bazuev <bazuka5801@mail.ru>
2023-01-25 23:46:22 +0300

    fix: telescope not find untracked files

--- lua/p-telescope.lua
+++ lua/p-telescope.lua
@@ -1,5 +1,7 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>f', builtin.git_files, {})
+vim.keymap.set('n', '<leader>f', function ()
+  builtin.git_files({ show_untracked = true })
+end, {})
 vim.keymap.set('n', '<leader>g', builtin.live_grep, {})
 
 local actions = require('telescope.actions')
